5ae1c35b8228d9eba79fec1f65b087d7203fdf0c
Iris Ferreira <iris.hf03@gmail.com>
2023-09-18 09:43:07 -0300

    Resolvendo exercicios CatalogoLivros e Ordenacaopessoa
    Signed-off-by: Iris Ferreira <iris.hf03@gmail.com>

---
+++ src/Pesquisa/Livro.java
@@ -0,0 +1,36 @@
+package Pesquisa;
+
+public class Livro {
+    //atributo
+    private String título;
+    private  String autor;
+    private int anoPublicacao;
+
+    public Livro(String título, String autor, int anoPublicacao) {
+        this.título = título;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTítulo() {
+        return título;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "título='" + título + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
